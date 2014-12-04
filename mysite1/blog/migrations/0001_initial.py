27e8b75e46eae8ae82621c0e3232ddf97870bb88
amankhatri <aman_khatri5b5@yahoo.com>
2014-12-04 17:52:26 -0600

    syncing db , make migrations add urls

---
+++ mysite1/blog/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('title', models.CharField(max_length=140)),
+                ('body', models.TextField()),
+                ('date', models.DateTimeField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
