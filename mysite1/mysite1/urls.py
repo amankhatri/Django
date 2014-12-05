9dcd488d12a554a9ffc73d46dd64c8331bf417b7
amankhatri <aman_khatri5b5@yahoo.com>
2014-12-04 19:40:18 -0600

    making archive and latestnews page

--- mysite1/mysite1/urls.py
+++ mysite1/mysite1/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite1.views.home', name='home'),
     url(r'^blog/', include('blog.urls')),
+    url(r'^latestnews/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
